89995c4ef47a1b4ba511ca30a5f1866d0a36c528
vijaysingh60 <121249393+vijaysingh60@users.noreply.github.com>
2024-12-31 16:57:00 +0530

    Update Login.js

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
         }
 
         try{
-            const responce = await axios.post("https://urban-nest-mygv.vercel.app/login",formData,{
+            const responce = await axios.post("https://localhost:3001/login",formData,{
                 header:{
                     'Content-Type' : 'application/json'
                 },
