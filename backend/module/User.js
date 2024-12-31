525db668a8c6355f0450ad52b65c5e4d5de00fda
vijaysingh61 <vsparihar7773@gmail.com>
2024-12-31 17:57:54 +0530

    Update User.js

--- backend/module/User.js
+++ backend/module/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 
 mongoose
-.connect(process.env.MONGO_URL)
+.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("connected")
 })
