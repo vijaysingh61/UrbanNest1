7b8f1f15db8c59012bd570c919f6e14dd8074fc6
vijaysingh60 <121249393+vijaysingh60@users.noreply.github.com>
2024-12-31 15:49:07 +0530

    Update AuthProvider.js

--- frontend/src/components/context/AuthProvider.js
+++ frontend/src/components/context/AuthProvider.js
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
 
     const checkAuthStatus = async () => {
         try {
-            const response = await axios.get('http://localhost:3001/api/check-auth', { withCredentials: true });
+            const response = await axios.get('https://urban-nest-mygv.vercel.app/api/check-auth', { withCredentials: true });
             setIsAuthenticated(response.data.authenticated);
         } catch (error) {
             setIsAuthenticated(false);
